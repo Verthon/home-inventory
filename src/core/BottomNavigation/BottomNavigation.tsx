b0bb03ff67449be461241b43523600648d88b70c
Krzysztof Sordyl <christopher.sordyl@gmail.com>
2022-07-04 18:25:11 +0200

    Feature/add basic routing and navbar (#7)
    * feat: add routing basic configuration and bottom navigation
    
    * feat: add mobile styling for bottom navbar
    
    * refactor: getters for elements

---
+++ src/core/BottomNavigation/BottomNavigation.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import { Link } from "react-router-dom";
+import { CreateIcon } from '../../icons/Create';
+import { HomeIcon } from '../../icons/Home';
+import { ListIcon } from '../../icons/List';
+
+import styles from './BottomNavigation.module.css'
+
+export const BottomNavigation = () => {
+  return <div className={styles.wrapper}>
+    <Link className={styles.link} to="/">
+      <HomeIcon />
+      <span>Home</span>
+    </Link>
+    <Link className={styles.link} to="/create">
+      <CreateIcon />
+      <span>Create</span>
+    </Link>
+    <Link className={styles.link} to="/list">
+      <ListIcon />
+      <span>List</span>
+    </Link>
+  </div>
+}
