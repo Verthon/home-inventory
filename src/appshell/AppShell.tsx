10f9025180a39737567e69e7907e9fed4fcd5813
Verthon <christopher.sordyl@gmail.com>
2022-07-04 22:06:09 +0200

    feat: add better config for code coverage

--- src/appshell/AppShell.tsx
+++ src/appshell/AppShell.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import { BottomNavigation } from 'src/core/BottomNavigation/BottomNavigation'
 
+import { BottomNavigation } from 'src/core/BottomNavigation/BottomNavigation'
 import { MainLayout } from 'src/layout/MainLayout/MainLayout'
 
 import type { AppShellProps } from './AppShell.types'
